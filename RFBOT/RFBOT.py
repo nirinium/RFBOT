8fbfb2da727db4e6bab8b0d93d17dfb512745a4a
Nirinium <wills.colton@gmail.com>
2018-08-17 22:46:33 -0400

    cmt1

--- RFBOT/RFBOT.py
+++ RFBOT/RFBOT.py
@@ -1,1 +1,28 @@
+import discord
+import json
+from discord import Game
+from discord.ext.commands import Bot
 
+BOT_PREFIX = (">")
+TOKEN = 'NDgwMTkzNzY0MzA5MDczOTIy.DlkScg.cdx33F83QtH7rLb9Unnce3WZk1Y'
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('>hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+#keep
+@client.event
+async def on_ready():
+    print('Logged in as | ' + client.user.name  + ' | ' + client.user.id)
+    print('------------------------------------------')
+
+#start client
+client.run(TOKEN)
